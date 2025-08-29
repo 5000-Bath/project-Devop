200661f9e0626ed8f34711ab8387dbb7d2b66953
Kongphop Kaochot <kongphop4239@gmail.com>
2025-08-29 23:26:21 +0700

    Lay out the structure

---
+++ Foodstore/src/page/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div style={{ padding: 24, background: "#f7f7f7", borderRadius: 12 }}>
+        <h1>Home Page</h1>
+        <p>Abubu</p>
+      </div>
+    );
+  }
+  
