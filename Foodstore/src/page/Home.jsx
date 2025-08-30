cc5493c4f49b7494896dc94257dd79bfe376f433
Weerapat Aphiphuwong <weerapat.aph@student.mahidol.edu>
2025-08-30 21:47:42 +0700

    Delete Foodstore directory

--- Foodstore/src/page/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-      <div style={{ padding: 24, background: "#f7f7f7", borderRadius: 12 }}>
-        <h1>Home Page</h1>
-        <p>Abubu</p>
-      </div>
-    );
-  }
-  
