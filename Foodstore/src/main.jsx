2f2d5ab37946d52bdbd92c90653344a2ffd0721e
First97 <yanaphatjum@gmail.com>
2025-09-10 17:04:34 +0700

    Login_Admin UI

--- Foodstore/src/main.jsx
+++ Foodstore/src/main.jsx
@@ -11,22 +11,31 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createBrowserRouter, RouterProvider, redirect,} from "react-router-dom";
 
 import App from "./App.jsx";
 import Home from "./page/Home.jsx";
 import About from "./page/About.jsx";
 import Contact from "./page/Contact.jsx";
+import Login from "./page/Login.jsx";
+
+const requireAuth = () => {
+  const authed = localStorage.getItem("auth") === "true";
+  if (!authed) return redirect("/login");
+  return null;
+};
 
 const router = createBrowserRouter([
+  { path: "/login", element: <Login /> },
   {
     path: "/",
     element: <App />,
+    loader: requireAuth, 
     children: [
-      { index: true, element: <Home /> },       // http://localhost:5173/
-      { path: "menu", element: <About /> },     // /menu
-      { path: "status", element: <Contact /> }, // /status
-      { path: "orders", element: <Home /> },    // /orders (ตัวอย่าง)
+      { index: true, element: <Home /> },      
+      { path: "menu", element: <About /> },     
+      { path: "status", element: <Contact /> }, 
+      { path: "orders", element: <Home /> },    
       { path: "setting", element: <div>Setting Page</div> },
       { path: "*", element: <div>Not Found</div> },
     ],
