a6f9afc8fbc14dbbd040f706a0dabb18f602fe25
Weerapat Aphiphuwong <weerapat.aph@student.mahidol.edu>
2025-09-27 16:02:32 +0700

    Update cypress.config.cjs

--- Foodstore_User/cypress.config.cjs
+++ Foodstore_User/cypress.config.cjs
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000", 
+    baseUrl: "http://192.168.31.231:3000", 
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
