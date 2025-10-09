81b78a18d3cc38d6641173d9cab7a79fc9089d09
First97 <yanaphatjum@gmail.com>
2025-10-09 09:40:24 +0700

    Update UpdateStatusRequest.java

--- firstapp/src/main/java/com/ecommerce/backend/dtos/UpdateStatusRequest.java
+++ firstapp/src/main/java/com/ecommerce/backend/dtos/UpdateStatusRequest.java
@@ -1,3 +1,6 @@
+package com.ecommerce.backend.dtos;
+import com.ecommerce.backend.models.OrderStatus;
+
 public class UpdateStatusRequest {
     private OrderStatus status;
     public OrderStatus getStatus() { return status; }
