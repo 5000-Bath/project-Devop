4d2adbdfdddd6a1b5e847edd7bca8338aced4947
Weerapat Aphiphuwong <weerapat.aph@student.mahidol.edu>
2025-10-15 18:01:47 +0700

    Update mysql-init-master.sql

--- k8s/mysql-init-master.sql
+++ k8s/mysql-init-master.sql
@@ -1,12 +1,4 @@
--- Master initialization
-CREATE DATABASE IF NOT EXISTS shopdb;
-CREATE USER IF NOT EXISTS 'shopuser'@'%' IDENTIFIED BY 'shoppassword';
-GRANT ALL PRIVILEGES ON shopdb.* TO 'shopuser'@'%';
-FLUSH PRIVILEGES;
-
--- Replication user
+-- สร้าง replication user
 CREATE USER IF NOT EXISTS 'repl'@'%' IDENTIFIED BY 'replpassword';
 GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
 FLUSH PRIVILEGES;
-
--- Binlog settings (server_id จะต้องตั้งใน deployment env)
