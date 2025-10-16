bd88db1dc4fca690ba45f1e72a6975793e87906f
Weerapat Aphiphuwong <weerapat.aph@student.mahidol.edu>
2025-10-16 16:26:03 +0700

    Update SecurityConfig.java

--- firstapp/src/main/java/com/ecommerce/backend/security/SecurityConfig.java
+++ firstapp/src/main/java/com/ecommerce/backend/security/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig {
                 "http://localhost:3000", "http://127.0.0.1:3000",
                 "http://localhost:3001", "http://127.0.0.1:3001",
                 "http://localhost:5173", "http://127.0.0.1:5173",
-                "http://localhost:4173", "http://127.0.0.1:4173"
+                "http://localhost:4173", "http://127.0.0.1:4173",
+                "http://10.10.10.8:31001"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization","Content-Type","Accept","Origin","X-Requested-With"));
