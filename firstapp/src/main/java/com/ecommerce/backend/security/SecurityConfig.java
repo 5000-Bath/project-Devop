59e20cb8169a45170feddc7087d92bef0ac193d2
Weerapat Aphiphuwong <weerapat.aph@student.mahidol.edu>
2025-10-17 00:07:21 +0700

    Update SecurityConfig.java

--- firstapp/src/main/java/com/ecommerce/backend/security/SecurityConfig.java
+++ firstapp/src/main/java/com/ecommerce/backend/security/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
                 "https://salaya.weerapatserver.com",
                 "adminrayong.weerapatserver.com",
                 "adminsalaya.weerapatserver.com",
+                "adminfoodstore.weerapatserver.com",
                 "http://localhost:3000", "http://127.0.0.1:3000",
                 "http://localhost:3001", "http://127.0.0.1:3001",
                 "http://localhost:5173", "http://127.0.0.1:5173",
